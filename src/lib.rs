89ce73a6ef653a441146e74a076f26e8543f4b70
Zhijie Xia <zhijiexiacs@gmail.com>
2022-08-15 16:07:11 -0600

    grep functionality provided

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,20 @@ use std::error::Error;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-   println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+    Ok(())
+}
 
-   Ok(())
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 pub struct Config {
@@ -28,4 +39,18 @@ impl Config {
    }
 }
 
+#[cfg(test)]
 
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
