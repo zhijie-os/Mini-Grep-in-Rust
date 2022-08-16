440fc3937c1e5fcad96fe4c7ac5d59fc92bf8515
Zhijie Xia <zhijiexiacs@gmail.com>
2022-08-16 15:56:46 -0600

    Replace Clone() with Interator

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use std::process;
 
 fn main() {
     // get arguments
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
